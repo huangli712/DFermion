16270b0bfca32de10f62ff5333ab1265e377cdfa
Li Huang <lihuang.dmft@gmail.com>
2019-04-29 00:40:22 +0800

    in debug mode

--- src/df_core.f90
+++ src/df_core.f90
@@ -40,6 +40,8 @@
 
      end select DF_CORE
 
+     call df_eval_latt() 
+
      return
   end subroutine df_run
 
@@ -315,12 +317,12 @@
 
      call df_dyson(-1, dual_g, dual_s, dual_b)
 
-     do w=1,nffrq
-         print *, w, fmesh(w)
-         print *, dual_s(w,1,:)
-     enddo
+     !!do w=1,nffrq
+     !!    print *, w, fmesh(w)
+     !!    print *, dual_s(w,1,:)
+     !!enddo
 
-     STOP
+     !!STOP
 
      deallocate(gstp)
      deallocate(gnew)
@@ -387,3 +389,42 @@
 
      return
   end subroutine df_cchi
+
+  subroutine df_eval_latt()
+     use constants
+     use control
+     use context
+
+     implicit none
+
+     integer :: i, j, k, w
+
+     do k=1,nkpts
+         do j=1,norbs
+             do i=1,nffrq
+                 latt_g(i,j,k) =  ( one / ( dmft_h(i,j) - ek(k) ) ) + &
+                                  ( one / ( dmft_h(i,j) - ek(k) ) ) / &
+                                  dmft_g(i,j) * dual_g(i,j,k) / dmft_g(i,j) * &
+                                  ( one / ( dmft_h(i,j) - ek(k) ) ) 
+             enddo ! over i={1,nffrq} loop
+         enddo ! over j={1,norbs} loop
+     enddo ! over k={1,nkpts} loop
+
+     !!do w=1,nffrq
+     !!    print *, w, fmesh(w)
+     !!    print *, latt_g(w,1,:)
+     !!enddo
+
+     do j=1,norbs
+         do i=1,nffrq
+             dmft_h(i,j) = dmft_h(i,j) + one / dmft_g(i,j) * sum(dual_g(i,j,:)) / sum(latt_g(i,j,:))
+         enddo
+     enddo
+
+     do w=1,nffrq
+         print *, w, fmesh(w), dmft_h(w,1)
+     enddo
+
+     STOP 'in df_eval_latt'
+     return
+  end subroutine df_eval_latt
