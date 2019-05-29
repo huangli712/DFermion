549f8178b296219bd9b8fc42cf42322efe1526bd
Li Huang <lihuang.dmft@gmail.com>
2019-05-29 20:32:24 +0800

    re-organize df_eval.f90

--- src/df_eval.f90
+++ src/df_eval.f90
@@ -1,19 +1,63 @@
 !!!-----------------------------------------------------------------------
 !!! project : azalea
-!!! program : df_eval_latt_g
+!!! program : df_eval_dmft_h
+!!!           df_eval_latt_g
 !!!           df_eval_latt_s
-!!!           df_eval_dmft_h
 !!!           df_eval_susc_c
 !!!           df_eval_susc_s
 !!! source  : df_eval.f90
 !!! type    : subroutines
 !!! author  : li huang (email:lihuang.dmft@gmail.com)
 !!! history : 04/29/2009 by li huang (created)
-!!!           04/30/2019 by li huang (last modified)
-!!! purpose : main subroutines for the dual fermion framework.
+!!!           05/29/2019 by li huang (last modified)
+!!! purpose : try to evaluate some key observables.
 !!! status  : unstable
 !!! comment :
 !!!-----------------------------------------------------------------------
+
+!!========================================================================
+!!>>> evaluate local observables                                       <<<
+!!========================================================================
+
+!!
+!! @sub df_eval_dmft_h
+!!
+!! calculate the local hybridization function within the dual fermion framework
+!!
+  subroutine df_eval_dmft_h()
+     use constants, only : one
+
+     use control, only : norbs
+     use control, only : nffrq
+
+     use context, only : fmesh
+     use context, only : dmft_g, dmft_h
+     use context, only : dual_g
+     use context, only : latt_g
+
+     implicit none
+
+! local variables
+! loop index for fermionic frequency \omega
+     integer :: i
+
+! loop index for orbitals
+     integer :: j
+
+     do j=1,norbs
+         do i=1,nffrq
+             associate ( zh => ( sum( dual_g(i,j,:) ) / sum( latt_g(i,j,:) ) ) )
+                 dmft_h(i,j) = dmft_h(i,j) + one / dmft_g(i,j) * zh 
+             end associate
+         enddo ! over i={1,nffrq} loop
+     enddo ! over j={1,norbs} loop
+
+     return
+  end subroutine df_eval_dmft_h
+
+!!========================================================================
+!!>>> evaluate lattice observables                                     <<<
+!!========================================================================
 
 !!
 !! @sub df_eval_latt_g
@@ -98,41 +142,9 @@
      return
   end subroutine df_eval_latt_s
 
-!!
-!! @sub df_eval_dmft_h
-!!
-!! calculate the local hybridization function within the dual fermion framework
-!!
-  subroutine df_eval_dmft_h()
-     use constants, only : one
-
-     use control, only : norbs
-     use control, only : nffrq
-
-     use context, only : fmesh
-     use context, only : dmft_g, dmft_h
-     use context, only : dual_g
-     use context, only : latt_g
-
-     implicit none
-
-! local variables
-! loop index for fermionic frequency \omega
-     integer :: i
-
-! loop index for orbitals
-     integer :: j
-
-     do j=1,norbs
-         do i=1,nffrq
-             associate ( zh => ( sum( dual_g(i,j,:) ) / sum( latt_g(i,j,:) ) ) )
-                 dmft_h(i,j) = dmft_h(i,j) + one / dmft_g(i,j) * zh 
-             end associate
-         enddo ! over i={1,nffrq} loop
-     enddo ! over j={1,norbs} loop
-
-     return
-  end subroutine df_eval_dmft_h
+!!========================================================================
+!!>>> evaluate q-dependent susceptibilities                            <<<
+!!========================================================================
 
 !!
 !! @sub df_eval_susc_c
