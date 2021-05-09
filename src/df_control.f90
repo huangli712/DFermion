14f85b01bd3eb072ca72d98af12403934add9952
Li Huang <lihuang.dmft@gmail.com>
2021-05-09 16:10:23 +0800

    add version to df_control.f90

--- src/df_control.f90
+++ src/df_control.f90
@@ -1,11 +1,12 @@
 !!!-----------------------------------------------------------------------
 !!! project : azalea
 !!! program : control module
+!!!           version module
 !!! source  : df_control.f90
 !!! type    : module
 !!! author  : li huang (email:lihuang.dmft@gmail.com)
 !!! history : 09/15/2009 by li huang (created)
-!!!           05/29/2019 by li huang (last modified)
+!!!           05/09/2021 by li huang (last modified)
 !!! purpose : define global control parameters for dual fermion framework.
 !!! status  : unstable
 !!! comment :
@@ -220,3 +221,66 @@
      integer, public, save :: cy     = 0
 
   end module control
+
+
+
+  module version
+     implicit none
+
+!!
+!! @var V_FULL
+!!
+!! version string, version number + date info. + status info.
+!!
+     character(len=20), public, parameter :: V_FULL = 'v0.0.2 @ 2021.05.09D'
+
+!!
+!! @var V_CURR
+!!
+!! version string, only version number
+!!
+     character(len=06), public, parameter :: V_CURR = 'v0.0.2'
+
+!!
+!! @var V_DATE
+!!
+!! version string, only date info.
+!!
+     character(len=11), public, parameter :: V_DATE = '2021.05.09'
+
+!!
+!! @var V_STAT
+!!
+!! version string, only status info., D means devel, T testing, R released.
+!!
+     character(len=01), public, parameter :: V_STAT = 'D'
+
+!!
+!! @var V_AUTH
+!!
+!! version string, author info.
+!!
+     character(len=11), public, parameter :: V_AUTH = 'by li huang'
+
+!!
+!! @var V_INST
+!!
+!! version string, affiliation info.
+!!
+     character(len=36), public, parameter :: V_INST = 'China Academy of Engineering Physics'
+
+!!
+!! @var V_MAIL
+!!
+!! version string, email info.
+!!
+     character(len=22), public, parameter :: V_MAIL = 'lihuang.dmft@gmail.com'
+
+!!
+!! @var V_GPL3
+!!
+!! version string, license info.
+!!
+     character(len=36), public, parameter :: V_GPL3 = 'GNU General Public License version 3'
+
+  end module version
