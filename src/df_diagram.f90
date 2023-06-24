ec14961edce4742d48d6e5ab2be28be8c4a5a91c
Li Huang <lihuang.dmft@gmail.com>
2023-06-24 09:52:09 +0800

    remove df_fft.f90

--- src/df_diagram.f90
+++ src/df_diagram.f90
@@ -195,3 +195,183 @@
 
      return
   end subroutine cat_dia_3d
+
+!!!-----------------------------------------------------------------------
+!!! project : dfermion @ azalea
+!!! program : cat_fft_1d
+!!!           cat_fft_2d
+!!!           cat_fft_3d
+!!! source  : df_fft.f90
+!!! type    : subroutines
+!!! author  : li huang (email:lihuang.dmft@gmail.com)
+!!! history : 10/01/2008 by li huang (created)
+!!!           06/24/2023 by li huang (last modified)
+!!! purpose : try to implement the fast fourier transformation.
+!!! status  : unstable
+!!! comment :
+!!!-----------------------------------------------------------------------
+
+!!
+!! note:
+!!
+!! require fftw3 software package
+!!
+
+!!
+!! @sub cat_fft_1d
+!!
+!! conduct fast fourier transformation in 1d
+!!
+  subroutine cat_fft_1d(op, nx, fin, fout)
+     use iso_c_binding
+     use constants, only : dp
+
+     implicit none
+
+     ! import fftw header file
+     include 'fftw3.f03'
+
+!! external arguments
+     ! fft direction, forward or backward
+     integer, intent(in) :: op
+
+     ! size of operand
+     integer, intent(in) :: nx
+
+     ! operand
+     complex(dp), intent(inout) :: fin(nx)
+     complex(dp), intent(inout) :: fout(nx)
+
+!! local variables
+     ! fftw descriptor handler
+     type(c_ptr) :: plan
+
+!! [body
+
+     select case (op)
+
+         case (+1)
+             plan = fftw_plan_dft_1d(nx, fin, fout, FFTW_FORWARD, FFTW_ESTIMATE)
+
+         case (-1)
+             plan = fftw_plan_dft_1d(nx, fin, fout, FFTW_BACKWARD, FFTW_ESTIMATE)
+
+         case default
+             call s_print_error('cat_fft_1d','unrecognized fft operation')
+
+     end select
+
+     call fftw_execute_dft(plan, fin, fout)
+     call fftw_destroy_plan(plan)
+
+!! body]
+
+     return
+  end subroutine cat_fft_1d
+
+!!
+!! @sub cat_fft_2d
+!!
+!! conduct fast fourier transformation in 2d
+!!
+  subroutine cat_fft_2d(op, nx, ny, fin, fout)
+     use iso_c_binding
+     use constants, only : dp
+
+     implicit none
+
+     ! import fftw header file
+     include 'fftw3.f03'
+
+!! external arguments
+     ! fft direction, forward or backward
+     integer, intent(in) :: op
+
+     ! size of operand
+     integer, intent(in) :: nx
+     integer, intent(in) :: ny
+
+     ! operand
+     complex(dp), intent(inout) :: fin(nx,ny)
+     complex(dp), intent(inout) :: fout(nx,ny)
+
+!! local variables
+     ! fftw descriptor handler
+     type(c_ptr) :: plan
+
+!! [body
+
+     select case (op)
+
+         case (+1)
+             plan = fftw_plan_dft_2d(nx, ny, fin, fout, FFTW_FORWARD, FFTW_ESTIMATE)
+
+         case (-1)
+             plan = fftw_plan_dft_2d(nx, ny, fin, fout, FFTW_BACKWARD, FFTW_ESTIMATE)
+
+         case default
+             call s_print_error('cat_fft_2d','unrecognized fft operation')
+
+     end select
+
+     call fftw_execute_dft(plan, fin, fout)
+     call fftw_destroy_plan(plan)
+
+!! body]
+
+     return
+  end subroutine cat_fft_2d
+
+!!
+!! @sub cat_fft_3d
+!!
+!! conduct fast fourier transformation in 3d
+!!
+  subroutine cat_fft_3d(op, nx, ny, nz, fin, fout)
+     use iso_c_binding
+     use constants, only : dp
+
+     implicit none
+
+     ! import fftw header file
+     include 'fftw3.f03'
+
+!! external arguments
+     ! fft direction, forward or backward
+     integer, intent(in) :: op
+
+     ! size of operand
+     integer, intent(in) :: nx
+     integer, intent(in) :: ny
+     integer, intent(in) :: nz
+
+     ! operand
+     complex(dp), intent(inout) :: fin(nx,ny,nz)
+     complex(dp), intent(inout) :: fout(nx,ny,nz)
+
+!! local variables
+     ! fftw descriptor handler
+     type(c_ptr) :: plan
+
+!! [body
+
+     select case (op)
+
+         case (+1)
+             plan = fftw_plan_dft_3d(nx, ny, nz, fin, fout, FFTW_FORWARD, FFTW_ESTIMATE)
+
+         case (-1)
+             plan = fftw_plan_dft_3d(nx, ny, nz, fin, fout, FFTW_BACKWARD, FFTW_ESTIMATE)
+
+         case default
+             call s_print_error('cat_fft_3d','unrecognized fft operation')
+
+     end select
+
+     call fftw_execute_dft(plan, fin, fout)
+     call fftw_destroy_plan(plan)
+
+!! body]
+
+     return
+  end subroutine cat_fft_3d
