f5077dc165a11a45448ae1c38113bdc3582579eb
Li Huang <lihuang.dmft@gmail.com>
2023-06-24 22:47:25 +0800

    merge df_fft.f90 and df_diagram.f90

--- src/df_diagram.f90
+++ src/df_diagram.f90
@@ -1,22 +1,29 @@
 !!!-----------------------------------------------------------------------
-!!! project : azalea
+!!! project : dfermion @ azalea
 !!! program : cat_dia_1d
 !!!           cat_dia_2d
 !!!           cat_dia_3d
+!!!           cat_fft_1d
+!!!           cat_fft_2d
+!!!           cat_fft_3d
 !!! source  : df_diagram.f90
 !!! type    : subroutines
 !!! author  : li huang (email:lihuang.dmft@gmail.com)
 !!! history : 10/01/2008 by li huang (created)
-!!!           04/17/2019 by li huang (last modified)
+!!!           06/24/2023 by li huang (last modified)
 !!! purpose : try to calculate the bubble diagram (convolution).
 !!! status  : unstable
 !!! comment :
 !!!-----------------------------------------------------------------------
 
+!!========================================================================
+!!>>> core subroutines                                                 <<<
+!!========================================================================
+
 !!
 !! @sub cat_dia_1d
 !!
-!! calculate the two-particle bubble diagram, 1d version
+!! calculate the two-particle bubble diagram, 1d version.
 !!
   subroutine cat_dia_1d(gin, ginp, chiq)
      use constants, only : dp
@@ -29,28 +36,30 @@
 
      implicit none
 
-! external arguments
-! G(\nu, K)
+!! external arguments
+     ! G(\nu, K)
      complex(dp), intent(in)  :: gin (nffrq,norbs,nkpts)
 
-! G(\nu + \omega, K + Q)
+     ! G(\nu + \omega, K + Q)
      complex(dp), intent(in)  :: ginp(nffrq,norbs,nkpts)
 
-! two-particle bubble diagram
+     ! two-particle bubble diagram
      complex(dp), intent(out) :: chiq(nffrq,norbs,nkpts)
 
-! local variables
-! loop index
+!! local variables
+     ! loop index
      integer :: i
      integer :: j
 
-! dummy complex(dp) variables
+     ! dummy complex(dp) variables
      complex(dp) :: gk(nkpts)
      complex(dp) :: gr(nkpts)
      complex(dp) :: g1(nkpts)
      complex(dp) :: g2(nkpts)
 
-! we have to make sure nkpts == nkp_x
+!! [body
+
+     ! we have to make sure nkpts == nkp_x
      call s_assert2(nkpts == nkp_x, 'nkpts != nkp_x')
 
      do i=1,norbs
@@ -71,13 +80,15 @@
      enddo ! over i={1,norbs} loop
      chiq = chiq / real(nkpts * beta)
 
+!! body]
+
      return
   end subroutine cat_dia_1d
 
 !!
 !! @sub cat_dia_2d
 !!
-!! calculate the two-particle bubble diagram, 2d version
+!! calculate the two-particle bubble diagram, 2d version.
 !!
   subroutine cat_dia_2d(gin, ginp, chiq)
      use constants, only : dp
@@ -90,28 +101,30 @@
 
      implicit none
 
-! external arguments
-! G(\nu, K)
+!! external arguments
+     ! G(\nu, K)
      complex(dp), intent(in)  :: gin (nffrq,norbs,nkpts)
 
-! G(\nu + \omega, K + Q)
+     ! G(\nu + \omega, K + Q)
      complex(dp), intent(in)  :: ginp(nffrq,norbs,nkpts)
 
-! two-particle bubble diagram
+     ! two-particle bubble diagram
      complex(dp), intent(out) :: chiq(nffrq,norbs,nkpts)
 
-! local variables
-! loop index
+!! local variables
+     ! loop index
      integer :: i
      integer :: j
 
-! dummy complex(dp) variables
+     ! dummy complex(dp) variables
      complex(dp) :: gk(nkpts)
      complex(dp) :: gr(nkpts)
      complex(dp) :: g1(nkpts)
      complex(dp) :: g2(nkpts)
 
-! we have to make sure nkpts == nkp_x * nkp_y
+!! [body
+
+     ! we have to make sure nkpts == nkp_x * nkp_y
      call s_assert2(nkpts == (nkp_x * nkp_y), 'nkpts != (nkp_x * nkp_y)')
 
      do i=1,norbs
@@ -132,13 +145,15 @@
      enddo ! over i={1,norbs} loop
      chiq = chiq / real(nkpts * nkpts * beta)
 
+!! body]
+
      return
   end subroutine cat_dia_2d
 
 !!
 !! @sub cat_dia_3d
 !!
-!! calculate the two-particle bubble diagram, 3d version
+!! calculate the two-particle bubble diagram, 3d version.
 !!
   subroutine cat_dia_3d(gin, ginp, chiq)
      use constants, only : dp
@@ -151,28 +166,30 @@
 
      implicit none
 
-! external arguments
-! G(\nu, K)
+!! external arguments
+     ! G(\nu, K)
      complex(dp), intent(in)  :: gin (nffrq,norbs,nkpts)
 
-! G(\nu + \omega, K + Q)
+     ! G(\nu + \omega, K + Q)
      complex(dp), intent(in)  :: ginp(nffrq,norbs,nkpts)
 
-! two-particle bubble diagram
+     ! two-particle bubble diagram
      complex(dp), intent(out) :: chiq(nffrq,norbs,nkpts)
 
-! local variables
-! loop index
+!! local variables
+     ! loop index
      integer :: i
      integer :: j
 
-! dummy complex(dp) variables
+     ! dummy complex(dp) variables
      complex(dp) :: gk(nkpts)
      complex(dp) :: gr(nkpts)
      complex(dp) :: g1(nkpts)
      complex(dp) :: g2(nkpts)
 
-! we have to make sure nkpts == nkp_x * nkp_y * nkp_z
+!! [body
+
+     ! we have to make sure nkpts == nkp_x * nkp_y * nkp_z
      call s_assert2(nkpts == (nkp_x * nkp_y * nkp_z), 'nkpts != (nkp_x * nkp_y * nkp_z)')
 
      do i=1,norbs
@@ -193,23 +210,14 @@
      enddo ! over i={1,norbs} loop
      chiq = chiq / real(nkpts * nkpts * nkpts * beta)
 
+!! body]
+
      return
   end subroutine cat_dia_3d
 
-!!!-----------------------------------------------------------------------
-!!! project : dfermion @ azalea
-!!! program : cat_fft_1d
-!!!           cat_fft_2d
-!!!           cat_fft_3d
-!!! source  : df_fft.f90
-!!! type    : subroutines
-!!! author  : li huang (email:lihuang.dmft@gmail.com)
-!!! history : 10/01/2008 by li huang (created)
-!!!           06/24/2023 by li huang (last modified)
-!!! purpose : try to implement the fast fourier transformation.
-!!! status  : unstable
-!!! comment :
-!!!-----------------------------------------------------------------------
+!!========================================================================
+!!>>> fast fourier transformation subroutines                          <<<
+!!========================================================================
 
 !!
 !! note:
